456b32d0ab713a624ab112638948af669a04c912
Jake Edwards <edwardsjake@live.com>
2024-01-09 14:23:32 -0600

    1.5 minus challenge

---
+++ solana-ping-frontend-starter/components/PingButton.tsx
@@ -0,0 +1,34 @@
+import { FC, useState } from 'react'
+import styles from '../styles/PingButton.module.css'
+import * as web3 from '@solana/web3.js'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+
+export const PingButton: FC = () => {
+	const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
+
+    const onClick = () => {
+        if (!connection || !publicKey) return;
+
+		const programId = new web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa");
+		const programDataAccount = new web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod");
+		const transaction = new web3.Transaction();
+
+		const ix = new web3.TransactionInstruction({
+			keys: [{ pubkey: programDataAccount, isSigner: false, isWritable: true}],
+			programId,
+		});
+
+		transaction.add(ix);
+		sendTransaction(transaction, connection).then((signature) => {
+			console.log(signature);
+		});
+    }
+    
+	return (
+		<div className={styles.buttonContainer} onClick={onClick}>
+			<button className={styles.button}>Ping!</button>
+		</div>
+	)
+}
+
