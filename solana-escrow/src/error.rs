4f6e18c43d9a7243e4c029167c50f22cb906c8c1
Jake Edwards <edwardsjake@live.com>
2024-01-19 17:12:19 -0600

    Initial structure

---
+++ solana-escrow/src/error.rs
@@ -0,0 +1,20 @@
+// error.rs (partially implemented)
+
+use solana_program::program_error::ProgramError;
+use thiserror::Error;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+    /// Not Rent Exempt
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
